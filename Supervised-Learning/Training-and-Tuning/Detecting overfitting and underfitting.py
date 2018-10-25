d2dfa80060e04f5ef19ed4598954b71640a2e5a3
Nikhil Jain <13591875+N1kh1l-Ja1n@users.noreply.github.com>
2018-10-25 19:08:54 +0530

    Update and rename Detecting overfitting and underfitting to Detecting overfitting and underfitting.py

---
+++ Supervised-Learning/Training-and-Tuning/Detecting overfitting and underfitting.py
@@ -0,0 +1,7 @@
+3 models were used in this assignment:
+- Decision Tree model
+- LR model
+- SVM model
+one of them overfits, one underfits and one is just right
+
+![Linear curves](/Downloads/learning-curves.png)
